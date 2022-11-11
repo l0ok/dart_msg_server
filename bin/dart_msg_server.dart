24c7e09601f7ab2dd836326a716967e301bec3a4
Max Look <maxlukianeko@gmail.com>
2022-11-11 08:47:57 +0200

    fc

--- bin/dart_msg_server.dart
+++ bin/dart_msg_server.dart
@@ -1,5 +1,19 @@
 import 'package:dart_msg_server/dart_msg_server.dart' as dart_msg_server;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_msg_server.calculate()}!');
+const String serverName = 'dartServer';
+const int port = 8080;
+
+Future<void> main() async {
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
+  print('server up:${server.address} :port ${server.port}');
+
+  var countUsers = 0;
+  await for (HttpRequest request in server) {
+    request.response.headers.set('dartServer', serverName);
+
+    request.response.write('hello ${++countUsers} + ${request.uri.path}');
+
+    await request.response.close();
+  }
 }
