8ad3c391c3c96bb12024a810de56f0f38242cabb
Max Look <maxlukianeko@gmail.com>
2022-11-11 08:40:08 +0200

    first commit

---
+++ bin/dart_msg_server.dart
@@ -0,0 +1,5 @@
+import 'package:dart_msg_server/dart_msg_server.dart' as dart_msg_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_msg_server.calculate()}!');
+}
---
+++ test/dart_msg_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_msg_server/dart_msg_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
